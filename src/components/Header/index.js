8e829ac9146df10f00e587c91101333418348052
vivianachoque <vivianaachoque@gmail.com>
2022-01-09 20:10:21 -0300

    ejercicio de orden y reestructuración de componentes resuelto

---
+++ src/components/Header/index.js
@@ -0,0 +1,11 @@
+import React from "react";
+
+const Header = () => {
+  const course = "Half Stack application development";
+  return (
+    <span>
+      <h1>{course}</h1>
+    </span>
+  );
+};
+export default Header;
